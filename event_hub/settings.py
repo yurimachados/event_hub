3a4b01b1edb5c5d7d2205936ae47624120a2f31c
Yuri Machado <ymachado1995@gmail.com>
2023-12-11 09:28:46 -0300

    Update SECRET_KEY retrieval method

--- event_hub/settings.py
+++ event_hub/settings.py
@@ -15,14 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+import os
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+load_dotenv()
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-409+wjdhy^h=g$!r&6xg0ckegzn(cs%2*^@fy7w3u6powk114)"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
